aa7af1b95da1de4d6cab99ce201651b1610c4388
chengfengzheng <chengfengzheng@yunify.com>
2019-08-12 21:52:00 +0800

    update docs, notes and language files

--- php/qingstor-setting-page.php
+++ php/qingstor-setting-page.php
@@ -1,117 +1,117 @@
-<div class="wrap">
-    <h1><?php _e('QingStor Settings', 'wp-qingstor'); ?></h1>
-    <h1 class="nav-tab-wrapper">
-        <a href="javascript:void(0)" class="nav-tab" onclick="tabs_switch(event, 'basic')" id="tab-title-basic"><?php _e('Bucket Settings', 'wp-qingstor'); ?></a>
-        <a href="javascript:void(0)" class="nav-tab" onclick="tabs_switch(event, 'upload')" id="tab-title-upload"><?php _e('Upload Settings', 'wp-qingstor'); ?></a>
-    </h1>
-    <form method="POST" action="">
-        <div id="tab-basic" class="div-tab">
-            <?php
-            if ($qingstor_error != QS_REQUEST_OK) {
-                qingstor_display_message('errors', $qingstor_error);
-            } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                if ($_REQUEST['upload_uploads']) {
-                    qingstor_display_message('upload_uploads');
-                } else {
-                    qingstor_display_message('settings');
+  <div class="wrap">
+      <h1><?php _e('QingStor Settings', 'wp-qingstor'); ?></h1>
+      <h1 class="nav-tab-wrapper">
+          <a href="javascript:void(0)" class="nav-tab" onclick="tabs_switch(event, 'basic')" id="tab-title-basic"><?php _e('Bucket Settings', 'wp-qingstor'); ?></a>
+          <a href="javascript:void(0)" class="nav-tab" onclick="tabs_switch(event, 'upload')" id="tab-title-upload"><?php _e('Upload Settings', 'wp-qingstor'); ?></a>
+      </h1>
+      <form method="POST" action="">
+          <div id="tab-basic" class="div-tab">
+              <?php
+                if ($qingstor_error != QS_REQUEST_OK) {
+                    qingstor_display_message('errors', $qingstor_error);
+                } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    if ($_REQUEST['upload_uploads']) {
+                        qingstor_display_message('upload_uploads');
+                    } else {
+                        qingstor_display_message('settings');
+                    }
                 }
-            }
-            ?>
-            <p>*<?php _e('The following items need to be created at ', 'wp-qingstor'); ?><a target="_blank" href="https://console.qingcloud.com/access_keys/"><?php _e('QingCloud Console', 'wp-qingstor'); ?></a><?php _e('.', 'wp-qingstor'); ?></p>
-            <table class="form-table">
-                <tbody>
-                    <tr>
-                        <th scope="row">
-                            <label for="access">ACCESS KEY ID</label>
-                        </th>
-                        <td>
-                            <input id="access" class="type-text regular-text" name="access_key" type="text" value="<?php echo $qingstor_access; ?>">
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-                            <label for="secret">SECRET ACCESS KEY</label>
-                        </th>
-                        <td>
-                            <input id="secret" class="type-text regular-text" name="secret_key" type="text" value="<?php echo $qingstor_secret; ?>">
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">
-                            <label for="bucket">Bucket <?php _e('Name', 'wp-qingstor'); ?></label>
-                        </th>
-                        <td>
-                            <input id="bucket" class="type-text regular-text" name="bucket_name" type="text" value="<?php echo $qingstor_bucket; ?>">
-                            <p class="description"><?php _e('Media files will be uploaded to the Bucket.', 'wp-qingstor'); ?></p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            <label for="set_policy"><?php _e('Automaticlly Set Policy', 'wp-qingstor'); ?></label>
-                        </th>
-                        <td>
-                            <input class="checkbox" type="checkbox" id="set_policy" name="set_policy" value="true" <?php echo $qingstor_set_policy ? "checked='checked'" : ''; ?>><?php _e('(Will set Acl as public read. If not necessary, do not change it.)', 'wp-qingstor'); ?>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div id="tab-upload" class="div-tab">
-            <h2><?php _e('Operations', 'wp-qingstor'); ?></h2>
-            <input id="upload_uploads" class="button button-primary" name="upload_uploads" value="<?php _e('Sync wp-content/uploads/ to Bucket', 'wp-qingstor'); ?>" type="submit">
-            <h2><?php _e('Settings', 'wp-qingstor'); ?></h2>
-            <div>
-                <table class="form-table">
-                    <tbody>
-                        <tr>
-                            <th scope="row">
-                                <label for="upload"><?php _e('File Types', 'wp-qingstor'); ?></label>
-                            </th>
-                            <td>
-                                <input id="upload" class="type-text regular-text" type="text" name="upload_types" value="<?php echo $qingstor_upload_types; ?>">
-                                <p class="description"><?php _e('File suffixes to upload.', 'wp-qingstor'); ?></p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">
-                                <label for="upload_prefix"><?php _e('Upload Prefix', 'wp-qingstor'); ?></label>
-                            </th>
-                            <td>
-                                <input id="upload_prefix" class="type-text regular-text" name="upload_prefix" type="text" value="<?php echo $qingstor_upload_prefix; ?>">
-                                <p class="description"><?php _e('Media Files will be uploaded to the directory.', 'wp-qingstor'); ?></p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">
-                                <label for="bucket_url">Bucket URL</label>
-                            </th>
-                            <td>
-                                <input id="bucket_url" class="type-text regular-text" name="bucket_url" type="text" value="<?php echo $qingstor_bucket_url; ?>">
-                                <p class="description"><?php _e('Bucket URL. If there is a CDN, please fill in according to the actual situation. (Should add http:// or https://)', 'wp-qingstor'); ?></p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <script>
-            function tabs_switch(evt, tabname) {
-                var i, tabcontent, tablinks;
-                tabcontent = document.getElementsByClassName("div-tab");
-                for (i = 0; i < tabcontent.length; i++) {
-                    tabcontent[i].style.display = "none";
-                }
-                tablinks = document.getElementsByClassName("nav-tab");
-                for (i = 0; i < tablinks.length; i++) {
-                    tablinks[i].className = tablinks[i].className.replace(" nav-tab-active", "");
-                }
-                document.getElementById("tab-" + tabname).style.display = "block";
-                evt.currentTarget.className += " nav-tab-active";
-            }
-            document.getElementById("tab-title-basic").click();
-        </script>
-        <p class="submit">
-            <input id="submit" class="button button-primary" name="submit" value="<?php _e('Save Changes', 'wp-qingstor'); ?>" type="submit">
-        </p>
-    </form>
-</div>
+                ?>
+              <p>*<?php _e('The following items need to be created at ', 'wp-qingstor'); ?><a target="_blank" href="https://console.qingcloud.com/access_keys/"><?php _e('QingCloud Console', 'wp-qingstor'); ?></a><?php _e('.', 'wp-qingstor'); ?></p>
+              <table class="form-table">
+                  <tbody>
+                      <tr>
+                          <th scope="row">
+                              <label for="access">ACCESS KEY ID</label>
+                          </th>
+                          <td>
+                              <input id="access" class="type-text regular-text" name="access_key" type="text" value="<?php echo $qingstor_access; ?>">
+                          </td>
+                      </tr>
+                      <tr>
+                          <th scope="row">
+                              <label for="secret">SECRET ACCESS KEY</label>
+                          </th>
+                          <td>
+                              <input id="secret" class="type-text regular-text" name="secret_key" type="text" value="<?php echo $qingstor_secret; ?>">
+                          </td>
+                      </tr>
+                      <tr>
+                          <th scope="row">
+                              <label for="bucket">Bucket <?php _e('Name', 'wp-qingstor'); ?></label>
+                          </th>
+                          <td>
+                              <input id="bucket" class="type-text regular-text" name="bucket_name" type="text" value="<?php echo $qingstor_bucket; ?>">
+                              <p class="description"><?php _e('Media files will be uploaded to the Bucket.', 'wp-qingstor'); ?></p>
+                          </td>
+                      </tr>
+                      <tr>
+                          <th>
+                              <label for="set_policy"><?php _e('Automaticlly Set Policy', 'wp-qingstor'); ?></label>
+                          </th>
+                          <td>
+                              <input class="checkbox" type="checkbox" id="set_policy" name="set_policy" value="true" <?php echo $qingstor_set_policy ? "checked='checked'" : ''; ?>><?php _e('(Will set Acl as public read. If not necessary, do not change it.)', 'wp-qingstor'); ?>
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <div id="tab-upload" class="div-tab">
+              <h2><?php _e('Operations', 'wp-qingstor'); ?></h2>
+              <input id="upload_uploads" class="button button-primary" name="upload_uploads" value="<?php _e('Sync wp-content/uploads/ to Bucket', 'wp-qingstor'); ?>" type="submit">
+              <h2><?php _e('Settings', 'wp-qingstor'); ?></h2>
+              <div>
+                  <table class="form-table">
+                      <tbody>
+                          <tr>
+                              <th scope="row">
+                                  <label for="upload"><?php _e('File Types', 'wp-qingstor'); ?></label>
+                              </th>
+                              <td>
+                                  <input id="upload" class="type-text regular-text" type="text" name="upload_types" value="<?php echo $qingstor_upload_types; ?>">
+                                  <p class="description"><?php _e('File suffixes to upload.', 'wp-qingstor'); ?></p>
+                              </td>
+                          </tr>
+                          <tr>
+                              <th scope="row">
+                                  <label for="upload_prefix"><?php _e('Upload Prefix', 'wp-qingstor'); ?></label>
+                              </th>
+                              <td>
+                                  <input id="upload_prefix" class="type-text regular-text" name="upload_prefix" type="text" value="<?php echo $qingstor_upload_prefix; ?>">
+                                  <p class="description"><?php _e('Media Files will be uploaded to the directory.', 'wp-qingstor'); ?></p>
+                              </td>
+                          </tr>
+                          <tr>
+                              <th scope="row">
+                                  <label for="bucket_url">Bucket URL</label>
+                              </th>
+                              <td>
+                                  <input id="bucket_url" class="type-text regular-text" name="bucket_url" type="text" value="<?php echo $qingstor_bucket_url; ?>">
+                                  <p class="description"><?php _e('Bucket URL. If there is a CDN, please fill in according to the actual situation. (Should add http:// or https://)', 'wp-qingstor'); ?></p>
+                              </td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+          </div>
+          <script>
+              function tabs_switch(evt, tabname) {
+                  var i, tabcontent, tablinks;
+                  tabcontent = document.getElementsByClassName("div-tab");
+                  for (i = 0; i < tabcontent.length; i++) {
+                      tabcontent[i].style.display = "none";
+                  }
+                  tablinks = document.getElementsByClassName("nav-tab");
+                  for (i = 0; i < tablinks.length; i++) {
+                      tablinks[i].className = tablinks[i].className.replace(" nav-tab-active", "");
+                  }
+                  document.getElementById("tab-" + tabname).style.display = "block";
+                  evt.currentTarget.className += " nav-tab-active";
+              }
+              document.getElementById("tab-title-basic").click();
+          </script>
+          <p class="submit">
+              <input id="submit" class="button button-primary" name="submit" value="<?php _e('Save Changes', 'wp-qingstor'); ?>" type="submit">
+          </p>
+      </form>
+  </div>
--- php/qingstor-upload.php
+++ php/qingstor-upload.php
@@ -1,11 +1,8 @@
 <?php
-
 define('QS_INVALID_BUCKET_URL', 31);
-
 final class QingStorUpload
 {
     private static $instance;
-
     public function __construct()
     {
         add_action('add_attachment', array($this, 'add_attachment'));
@@ -15,7 +12,6 @@ final class QingStorUpload
         add_filter('upload_dir', array($this, 'upload_dir'));
         add_filter('intermediate_image_sizes_advanced', array($this, 'intermediate_image_sizes_advanced'));
     }
-
     public static function get_instance()
     {
         if (!(self::$instance instanceof QingStorUpload)) {
@@ -23,7 +19,6 @@ final class QingStorUpload
         }
         return self::$instance;
     }
-
     /**
      * Hook function, auto Sync to QingStor Bucket when upload Media files to WordPress.
      * @param string $post_ID
@@ -36,7 +31,6 @@ final class QingStorUpload
         $data = array('file' => end(explode($wp_upload_dir['basedir'] . '/', $file_path)));
         $this->upload_data($data);
     }
-
     /**
      * Hook function, delete the file on QingStor Bucket.
      * @param string $post_id
@@ -54,7 +48,6 @@ final class QingStorUpload
         $object = $options['upload_prefix'] . $filename;
         $bucket->deleteObject($object);
     }
-
     public function get_files_local_and_remote($dirname, $basedir, $prefix)
     {
         $dir = opendir($dirname);
@@ -72,7 +65,6 @@ final class QingStorUpload
         closedir($dir);
         return $files;
     }
-
     /**
      * Upload all files in wp-content/uploads to QingStor Bucket.
      */
@@ -87,12 +79,10 @@ final class QingStorUpload
         $this->scheduled_upload_files($files);
         return QS_REQUEST_OK;
     }
-
     public function scheduled_upload_files($local_remote_path)
     {
         wp_schedule_single_event(time() + 1, 'qingstor_scheduled_upload_hook', array($local_remote_path));
     }
-
     /**
      * Upload multiple files.
      * @param array $local_remote_path Array(key: localpath, value: remote_path).
@@ -106,7 +96,6 @@ final class QingStorUpload
         if (empty($bucket = qingstor_get_bucket())) {
             return QS_CLIENT_ERROR;
         }
-
         foreach ($local_remote_path as $local_path => $remote_path) {
             if (file_exists($local_path)) {
                 /**
@@ -121,7 +110,6 @@ final class QingStorUpload
                     $nparts = 0;
                     // Use md5 value of the first 512 bytes of the file for etag.
                     $etag   = md5(file_get_contents($local_path, null, null, 0, 512));
-
                     $res = $bucket->initiateMultipartUpload($remote_path);
                     if (($ret = qingstor_http_status($res)) != QS_REQUEST_OK) {
                         return $ret;
@@ -173,7 +161,6 @@ final class QingStorUpload
                         )
                     );
                 }
-
                 // delete the file after uploading successfully
                 if ($res_upload_qingstor->statusCode == 201) {
                     wp_schedule_single_event(time() + 1, 'delete_local_file', array($local_path));
@@ -181,7 +168,6 @@ final class QingStorUpload
             }
         }
     }
-
     /**
      * Upload files.
      * @param array $data array with the key 'file'
@@ -191,15 +177,12 @@ final class QingStorUpload
         $wp_upload_dir = wp_get_upload_dir();
         $bucket = qingstor_get_bucket();
         $upload_prefix = get_option('qingstor-options')['upload_prefix'];
-
         if (empty($upload_prefix) || empty($bucket)) {
             return;
         }
-
         $files[$wp_upload_dir['basedir'] . '/' . $data['file']] = $upload_prefix . $data['file'];
         $this->upload_files($files);
     }
-
     /**
      * Hook function, delete the file after uploading to QingStor Bucket.
      * @param string $local_path The local path of the original file of the uploaded file.
@@ -210,7 +193,6 @@ final class QingStorUpload
             unlink($local_path);
         }
     }
-
     /**
      * Hook function, set url and baseurl of media file.
      * @param array $uploads Array includes path, basedir, subdir, url, baseurl.
@@ -223,7 +205,6 @@ final class QingStorUpload
         $uploads['baseurl'] = $options['bucket_url'] . substr($options['upload_prefix'], 0, -1);
         return $uploads;
     }
-
     /**
      * Hook function, set to abandon genarate thumbnail.
      * @param array $sizes Array includes information about the file's size.
@@ -235,5 +216,4 @@ final class QingStorUpload
         return $sizes;
     }
 }
-
 QingStorUpload::get_instance();
